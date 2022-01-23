d912c9e81dd5ed65c6145fe373f5dafe94ea5019
Erick Narvaez <82183750+ggquicky@users.noreply.github.com>
2022-01-23 15:37:29 -0600

    add navbar.js

---
+++ src/navbar/navbar.js
@@ -0,0 +1,16 @@
+function Navbar(){
+return(
+    <nav>
+        <ul>
+            <li><a href=" " ><i class="fas fa-home"></i>Home</a></li>
+            <li><a href=" " ><i class="far fa-file-code"></i>My Work</a></li>
+            <li><a href=" " ><i class="far fa-address-card"></i>Contact me</a></li>
+            
+        </ul>
+    </nav>
+);
+
+}
+
+
+export default Navbar;
