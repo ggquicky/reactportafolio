7ce9094da5932ba3668b75e1cb91cfe01fafa42f
Erick Narvaez <82183750+ggquicky@users.noreply.github.com>
2022-02-08 14:23:03 -0600

    update the css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+
 import Navbar from './navbar/navbar';
 
 function App() {
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -2,11 +2,11 @@ import './navbar.css';
 
 function Navbar(){
 return(
-    <nav class= "nav-bar">
+    <nav className= "nav-bar">
         <ul>
-            <li><a href=" " ><i class="fas fa-home"></i>Home</a></li>
-            <li><a href=" " ><i class="far fa-file-code"></i>My Work</a></li>
-            <li><a href=" " ><i class="far fa-address-card"></i>Contact me</a></li>
+            <li><a href=" " ><i className="fas fa-home"></i>Home</a></li>
+            <li><a href=" " ><i className="far fa-file-code"></i>My Work</a></li>
+            <li><a href=" " ><i className="far fa-address-card"></i>Contact me</a></li>
             
         </ul>
     </nav>
