bd84d45b9811b5306e3d00b182bc6d0b2e718abe
Erick Narvaez <82183750+ggquicky@users.noreply.github.com>
2022-02-08 22:08:00 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 
 import Navbar from './navbar/navbar';
+import About from './about/about';
 
 function App() {
   return (
     <div className="App">
       
-        <Navbar/>
-      
+        <About />
+
+        
     </div>
   );
 }
---
+++ src/about/about.js
@@ -0,0 +1,26 @@
+
+function About(){
+    return(
+        <div className= "sectionAbout" id="sectionAbout">
+            <div className="mobile">
+            <img src='./profile.jpeg' alt="profile"/>
+            <h1>Hi, I`m Erick Narvaez</h1>
+            <h2>a starting web developer</h2>
+
+            </div>
+            <div className="desktop">
+                <p>I´m starting a new journey in the web developer career. 
+                    This new journey has a lot of challeges. 
+                    All you need is creativity and some code to made excellent web pages.
+                    My first goal is being a Frontend Developer.
+                </p>
+
+            </div>
+
+        </div>
+    );
+    
+    }
+    
+    
+    export default About;
