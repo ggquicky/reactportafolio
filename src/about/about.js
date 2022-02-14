f5f9566d927bb661457f861443a00981b066a640
Erick Narvaez <82183750+ggquicky@users.noreply.github.com>
2022-02-13 21:14:28 -0600

    done

--- src/about/about.js
+++ src/about/about.js
@@ -7,16 +7,20 @@ export default class about extends Component {
   render() {
     return (
       <div className="sectionAbout" id="sectionAbout">
-        <img className="profilePic" src={profileP} alt="profile" />
-        <h1>Hi, I`m Erick Narvaez</h1>
-        <h2>a starting web developer</h2>
-          
-        <p>
-          I´m starting a new journey in the web developer career. This new
-          journey has a lot of challeges. All you need is creativity and some
-          code to made excellent web pages. My first goal is being a Frontend
-          Developer.
-        </p>
+        <div className="aboutHeader">
+          <img className="profilePic" src={profileP} alt="profile" />
+          <h1>Hi, I`m Erick Narvaez</h1>
+          <h2>a starting web developer</h2>
+        </div>
+
+        <div>
+          <p>
+            I´m starting a new journey in the web developer career. This new
+            journey has a lot of challeges. All you need is creativity and some
+            code to made excellent web pages. My first goal is being a Frontend
+            Developer.
+          </p>
+        </div>
       </div>
     );
   }
