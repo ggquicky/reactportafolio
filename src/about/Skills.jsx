0d4596452557af221cf232442177d582c045cc0f
ggquicky <ggquicky@gmail.com>
2022-09-12 11:42:41 -0600

    fixing CSS

--- src/about/Skills.jsx
+++ src/about/Skills.jsx
@@ -1,15 +1,24 @@
 import { Flex, Icon, Text } from "@chakra-ui/react";
 import { FaReact, FaVuejs, FaLaravel } from "react-icons/fa";
-import { SiChakraui, SiTailwindcss } from "react-icons/si";
+import {
+  SiChakraui,
+  SiJavascript,
+  SiPhp,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
 
 const skills = [
-  { name: "React", icon: FaReact },
-  { name: "VUE", icon: FaVuejs },
-  { name: "NEXTjs", icon: TbBrandNextjs },
-  { name: "LARAVEL", icon: FaLaravel },
-  { name: "Tailwind", icon: SiTailwindcss },
-  { name: "Chakra UI", icon: SiChakraui },
+  { name: "JavaScript", icon: SiJavascript, color: "#f7e018" },
+  { name: "TypeScript", icon: SiTypescript, color: "#2f74c0" },
+  { name: "Tailwind", icon: SiTailwindcss, color: "#07b0ce" },
+  { name: "Chakra UI", icon: SiChakraui, color: "#41c7bf" },
+  { name: "React", icon: FaReact, color: "#53c1de" },
+  { name: "VUE", icon: FaVuejs, color: "#4bb483" },
+  { name: "NEXTjs", icon: TbBrandNextjs, color: "white" },
+  { name: "PHP", icon: SiPhp, color: "white" },
+  { name: "LARAVEL", icon: FaLaravel, color: "#ff291a" },
 ];
 
 export default function Skills() {
@@ -22,7 +31,7 @@ export default function Skills() {
           justifyContent="center"
           align="center"
           gap="8px"
-          _hover={{ color: "white" }}
+          _hover={{ color: skill.color }}
         >
           <Icon h="30px" w="30px" as={skill.icon} />
           <Text key={index}>{skill.name}</Text>
