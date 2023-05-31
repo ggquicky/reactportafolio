5b94fab33f5d982e25cd2e9fc8774ae62ca2ecb3
Erick Narvaez <ggquicky@gmail.com>
2023-05-31 17:55:06 -0600

    added camera control

--- src/controls/Controls.jsx
+++ src/controls/Controls.jsx
@@ -1,12 +1,11 @@
-import {useThree} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
 
 
 export function Controls () {
-    const camera = useThree(scene => scene.camera)
+
 
 
     return (
-        <OrbitControls onChange={()=>{console.log(camera.position)}}  makeDefault/>
+        <OrbitControls   makeDefault/>
     )
 }
